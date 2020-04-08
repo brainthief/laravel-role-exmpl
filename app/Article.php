dd6ba3e84e1968470273b4a99eeb8c23dcb49293
brainthief <vytautas1983@gmail.com>
2020-04-08 22:37:50 +0300

    only owner articles - global scope

--- app/Article.php
+++ app/Article.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $fillable = ['title', 'full_text', 'category_id', 'user_id'];
+ protected $fillable = ['title', 'full_text', 'category_id', 'user_id'];
+
+ protected static function booted()
+ {
+  static::addGlobalScope('user', function (Builder $builder) {
+   $builder->where('user_id', auth()->id());
+  });
+ }
 }
